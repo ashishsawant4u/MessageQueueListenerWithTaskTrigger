528edd51b01e7996ed3d0da8a1da0e2d14d56325
ashish.sawant-ext@rexel.com <c2Wr4w2H>
2020-07-25 20:14:24 +0530

    Spring Project with dependency task/steam rabbitmq

---
+++ src/main/java/com/boot/demo/MessageQueueListenerWithTaskTriggerApplication.java
@@ -0,0 +1,13 @@
+package com.boot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageQueueListenerWithTaskTriggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageQueueListenerWithTaskTriggerApplication.class, args);
+	}
+
+}
